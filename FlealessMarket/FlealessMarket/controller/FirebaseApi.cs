d1255ddedc2cdd725d85526c8a044dc7d6ef74cd
dreese33 <dylanjacobreese@gatech.edu>
2019-11-17 14:43:18 -0500

    Modified file structure

---
+++ FlealessMarket/FlealessMarket/controller/FirebaseApi.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Firebase.Database;
+using Firebase.Storage;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace FlealessMarket
+{
+    //CRUD style API for firebase database
+    public class FirebaseApi : ContentPage
+    {
+        //Firebase client
+        public static FirebaseClient firebaseClient;
+        private String auth = "VbwAboESljcPR5t2O5VXCpsDyyHvMPqoPAUh3Dte";
+        public static String url = "https://flealess-market.firebaseio.com/";
+
+        public static FirebaseStorage firebaseStorage;
+
+        private String imageUrl = "flealess-market.appspot.com";
+
+        //Login globals
+        public static int LoginStatus
+        {
+            get { return _loginStatus; }
+            set
+            {
+                _loginStatus = value;
+                if (_loginStatus == 1)
+                {
+                    //User
+                    Application.Current.MainPage = new NavigationPage(new UserHome());
+                }
+                else if (_loginStatus == 2)
+                {
+                    //Consignment
+                    Application.Current.MainPage = new NavigationPage(new Home());
+                } else if (_loginStatus == 3)
+                {
+                    //Driver
+                    Application.Current.MainPage = new DriverHome();
+                }
+                else
+                {
+                    Application.Current.MainPage = new MainPage();
+                }
+            }
+        }
+
+        private static int _loginStatus = 0;
+
+        public FirebaseApi()
+        {
+            //Initialize firebase client
+            FirebaseApi.firebaseClient = new FirebaseClient(FirebaseApi.url, new FirebaseOptions
+            {
+                AuthTokenAsyncFactory = () => Task.FromResult(this.auth)
+            });
+
+            //Initialize firebase storage
+            FirebaseApi.firebaseStorage = new FirebaseStorage(this.imageUrl);
+        }
+    }
+}
---
+++ FlealessMarket/FlealessMarket/view/User/UserHome.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FlealessMarket
+{
+    public partial class UserHome : ContentPage
+    {
+        public UserHome()
+        {
+            InitializeComponent();
+        }
+
+        //Throw item out
+        private void Throw_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new DonateItem());
+        }
+
+        private void Sell_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MapPage());
+        }
+    }
+}
